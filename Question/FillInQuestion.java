6cfa39b9f9c64eb0549f031c229edf547a92d1e5
Geoff Schmit <gcschmit@gmail.com>
2020-01-30 09:35:02 -0600

    began defining FillInQuestion subclass

---
+++ Question/FillInQuestion.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * A FillInQuestion is constructed with a string that contains the answer surrounded by '_'.
+ *      For example, "The inventor of Java is _James Gosling_." This question should be displayed
+ *      as: The inventor of Java is ______.
+ *
+ * @author gcschmit
+ * @version 30 January 2020
+ */
+
+/*
+ * The FillInQuestion class extends (i.e., is a subclass of) the Question class.
+ */
+public class FillInQuestion extends Question
+{
+    /*
+     * Do not declare instance variables for text and answer! The text and answer instance variables
+     *      are inherited from the Question class!
+     */
+    
+    /**
+     * Constructs a FillInQuestion object with the specified text that contains the answer.
+     * 
+     * @param question  the specified question text with the embedded answer
+     */
+    public FillInQuestion(String question)
+    {
+        
+    }
+}
+
+
+
+
