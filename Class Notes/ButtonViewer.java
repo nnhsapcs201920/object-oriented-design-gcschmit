66d4c40f9e1e1a24f19c2fe394d89d2963c28b2c
Geoff Schmit <gcschmit@gmail.com>
2020-02-11 13:09:09 -0600

    second increment of ButtonViewer GUI
    label added; inner class

--- Class Notes/ButtonViewer.java
+++ Class Notes/ButtonViewer.java
@@ -2,7 +2,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This class demonstrates how to create a GUI with a button and an action listener.
@@ -14,35 +15,39 @@ public class ButtonViewer
 {
     private final int FRAME_WIDTH = 400;
     private final int FRAME_HEIGHT = 100;
-    
+
     private JFrame frame;
     private JPanel panel;
     private JButton button;
-    
+    private JLabel label;
+
+    private int clickCount;
 
     /**
      * Constructor for objects of class ButtonViewer
      */
     public ButtonViewer()
     {
+        this.clickCount = 0;
+
         // 1. define and setup the UI components
         this.frame = new JFrame();
         this.panel = new JPanel();
-        
+
         this.button = new JButton("Click Me!");
         this.panel.add(this.button);
-        
+
+        this.label = new JLabel("0 clicks");
+        this.panel.add(this.label);
+
         this.frame.add(this.panel);
-        
-        
+
         // 2. create listener objects
         ClickListener listener = new ClickListener();
-        
-        
+
         // 3. register listener objects with components that generate events
         this.button.addActionListener(listener);
-        
-        
+
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setVisible(true);
@@ -51,5 +56,15 @@ public class ButtonViewer
     public static void main(String[] args)
     {
         ButtonViewer viewer = new ButtonViewer();
+    }
+
+    private class ClickListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent event)
+        {
+            clickCount++;
+            label.setText(clickCount + " clicks");
+        }
     }
 }
