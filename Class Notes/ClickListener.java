649deca0f1ad4a15181e6e4c2df5f67d3028801b
Geoff Schmit <gcschmit@gmail.com>
2020-02-11 10:12:39 -0600

    first increment of ButtonViewer GUI

---
+++ Class Notes/ClickListener.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Write a description of class ClickListener here.
+ *
+ * @author gcschmit
+ * @version 11feb2020
+ */
+public class ClickListener implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("button clicked");
+    }
+}
